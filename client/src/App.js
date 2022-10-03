77778e2ed39aebf46b3af6d39c51edef8ab85770
Everett Cooley <39889137+EverettCooley@users.noreply.github.com>
2022-10-03 14:42:35 -0700

    Intial Commit

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React, { Fragment } from "react";
+import ListBlogs from "./blogs";
+
+function App() {
+    return (
+        <Fragment>
+            <div className="container">
+                <ListBlogs />
+            </div>
+        </Fragment>
+    );
+}
+
+export default App;
