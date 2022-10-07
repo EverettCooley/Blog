b4812e482e198d135d2b878d67a9db1280b104b7
Everett Cooley <39889137+EverettCooley@users.noreply.github.com>
2022-10-07 00:52:34 -0700

    Intial commit

--- server/server.js
+++ server/server.js
@@ -1,22 +1,20 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const pool = require("./db");
+const db = require("./db");
 
+// middleware
 app.use(cors());
 app.use(express.json());
 
 // routes
-
 // add title
 app.post("/newPost", async (req, res) => {
-    // req.body = {
-    //     title: "watermelons are better",
-    // };
     try {
         console.log(req.body);
         const { title, content } = req.body;
-        const newBlog = await pool.query(
+        const newBlog = await db.query(
             "INSERT INTO blogs(title, content) VALUES($1, $2) RETURNING *",
             [title, content]
         );
@@ -30,13 +28,13 @@ app.post("/newPost", async (req, res) => {
 // get all titles
 app.get("/blogs", async (req, res) => {
     try {
-        const allTitles = await pool.query("SELECT * FROM blogs");
+        const allTitles = await db.query("SELECT * FROM blogs");
         res.json(allTitles.rows);
     } catch (err) {
         console.error(err.message);
     }
 });
-
-app.listen(5001, () => {
-    console.log("server has started on port 5001");
+const port = process.env.PORT || 5001;
+app.listen(port, () => {
+    console.log("server has started");
 });
